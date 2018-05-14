a149ec42c7c0db75090ad5d35c371e9170e1be58
ArielBinenfeld <Ariel.Binenfeld@gmail.com>
2018-05-14 19:49:25 +0300

    minor changes

--- hw2/q1.py
+++ hw2/q1.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from scipy import ndimage
 import scipy.ndimage.filters as filters
 import cv2 as cv
+import imageio as imio
 
 
 def display_images(image_list):
@@ -21,36 +22,36 @@ def laplacian_decompose(image, levels):
 
     previous_filtered = image
     previous_sigma = 2
+    kernel_size = 21
     for i in range(0, levels-1):
         print(i)
         current_sigma = previous_sigma * 2
-        # current_filtered = filters.gaussian_filter(image, current_sigma, mode='wrap')
-        kernel_size = 5*current_sigma + 1
-        current_filtered = cv.GaussianBlur(src=image, ksize=(kernel_size, kernel_size), sigmaX=current_sigma)
+        current_filtered = filters.gaussian_filter(image, current_sigma)
+        #kernel_size = 5*current_sigma + 1
+        #current_filtered = cv.GaussianBlur(src=image, ksize=(kernel_size, kernel_size), sigmaX=current_sigma)
         print("curr sigma == " + str(current_sigma))
         sub_res = previous_filtered - current_filtered
-        cv.namedWindow("TEST", cv.WINDOW_AUTOSIZE)
-        cv.imshow("TEST", sub_res, )
-        cv.waitKey()
-        '''
-        plt.gray()
-        plt.figure()
-        plt.imshow(sub_res)
-        plt.show()
-        '''
+        # cv.namedWindow("TEST", cv.WINDOW_AUTOSIZE)
+        # cv.imshow("TEST", sub_res, )
+        # cv.waitKey()
+
         f_images.append(sub_res)
         previous_sigma = current_sigma
         previous_filtered = current_filtered
-    f_images.append(filters.gaussian_filter(image, previous_sigma * 2))
+    f_images.append(previous_filtered)
 
     return f_images
 
+def reconstruct_from_laplcian_piramid(lpiramid):
+    
+
+
 
 if __name__ == "__main__":
-    # image = ndimage.imread(fname="our_imgs/vettel.jpg", mode="L")
-    image = cv.imread('data/Inputs/imgs/0006_001.png')
-    image = cv.cvtColor(image, cv.COLOR_BGR2GRAY) # For our own RGB input
-    cv.imshow("original", image)
+    image = ndimage.imread(fname='data/Inputs/imgs/0006_001.png', mode="L")
+    # image = cv.imread('data/Inputs/imgs/0006_001.png')
+    # image = cv.cvtColor(image, cv.COLOR_BGR2GRAY) # For our own RGB input
+    # cv.imshow("original", image)
     laplacian_pyramid = laplacian_decompose(image, 5)
     # pyramid = tuple(pyramid_gaussian(image, downscale=2, multichannel=True)) # needs more imports
     print('end')
