ed9b9f4d10b173f9619a7c31d7d3486234530d5e
ArielBinenfeld <Ariel.Binenfeld@gmail.com>
2018-05-14 18:38:28 +0300

    Minor changes + HW data uploaded

--- hw2/q1.py
+++ hw2/q1.py
@@ -25,7 +25,7 @@ def laplacian_decompose(image, levels):
         print(i)
         current_sigma = previous_sigma * 2
         # current_filtered = filters.gaussian_filter(image, current_sigma, mode='wrap')
-        kernel_size = 10*current_sigma + 1
+        kernel_size = 5*current_sigma + 1
         current_filtered = cv.GaussianBlur(src=image, ksize=(kernel_size, kernel_size), sigmaX=current_sigma)
         print("curr sigma == " + str(current_sigma))
         sub_res = previous_filtered - current_filtered
@@ -48,8 +48,9 @@ def laplacian_decompose(image, levels):
 
 if __name__ == "__main__":
     # image = ndimage.imread(fname="our_imgs/vettel.jpg", mode="L")
-    image = cv.imread("our_imgs/cameraman.jpg")
-    image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    image = cv.imread('data/Inputs/imgs/0006_001.png')
+    image = cv.cvtColor(image, cv.COLOR_BGR2GRAY) # For our own RGB input
+    cv.imshow("original", image)
     laplacian_pyramid = laplacian_decompose(image, 5)
     # pyramid = tuple(pyramid_gaussian(image, downscale=2, multichannel=True)) # needs more imports
     print('end')
