5180b006d5345cd4cce5f314b4d71e8c6f37e63d
BerIgor <bigor11@gmail.com>
2018-05-25 20:38:54 +0300

    Segmentation works

--- hw2/q3_video_fgbg.py
+++ hw2/q3_video_fgbg.py
@@ -19,21 +19,23 @@ import pytorch_segmentation_detection.models.resnet_dilated as resnet_dilated
 
 
 model_path = 'C:/GitProjects/vision/hw2/q3/pytorch_segmentation_detection/recipes/pascal_voc/segmentation/resnet_34_8s_68.pth'
+video_path = 'C:/GitProjects/vision/hw2/our_data/ariel.mp4'
 
 
 def image_get_fg_mask(image):
     fcn = resnet_dilated.Resnet34_8s(num_classes=21)
     fcn.load_state_dict(torch.load(model_path, map_location=lambda storage, loc: storage))
     fcn.eval()
-    cvshow("orig", image)
     image = prep_image(image)
     nn_result = fcn(image)
-    cvshow(image)
     _, tmp = nn_result.squeeze(0).max(0)
     segmentation = tmp.data.cpu().numpy().squeeze()
-
+    print(np.shape(segmentation))
+    mask = np.zeros(np.shape(segmentation))
+    mask[segmentation == 15] = 1
     plt.imshow(segmentation)
     plt.show()
+    return mask
 
 
 def video_to_frames(video_name):
@@ -59,30 +61,26 @@ def prep_image(image):
     return Variable(timage)
 
 
-def display_images(image_list):
-    f, sps = plt.subplots(nrows=1, ncols=len(image_list))
-    # plt.gray()
-
-    for i in range(0, len(image_list)):
-        sps[i].imshow(image_list[i])
-        sps[i].axis('off')
-
-    plt.show()
-    return
-
-
 def cvshow(title, im):
     cv.namedWindow(title, cv.WINDOW_KEEPRATIO | cv.WINDOW_NORMAL)
     cv.imshow(title, im)
     cv.waitKey()
 
 
+def apply_mask(image, mask):
+    mask_rgb = np.repeat(mask[:, :, np.newaxis], 3, axis=2)
+    masked_image = np.multiply(image, mask_rgb)
+    return masked_image
+
+
 if __name__ == "__main__":
     print("Welcome to q3")
-    video_fname = 'our_data/cup.mov'
-    frame = video_to_frames(video_fname)
+    # frame = video_to_frames(video_path)
+    frame = cv.imread('our_data/GAzE2.jpeg')
     # nn_test(frame)
-    image_get_fg_mask(frame)
+    mask = image_get_fg_mask(frame)
+    masked = apply_mask(frame, mask)
+    cvshow("mask", masked)
     print("Finished")
     # image_get_fg_mask(frame)
 
