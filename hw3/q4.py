0aa380894f2a0ec0b569173f7370ecf945c16601
BerIgor <bigor11@gmail.com>
2018-06-08 23:08:00 +0300

    Transformation calculation works

--- hw3/q4.py
+++ hw3/q4.py
@@ -1,19 +1,32 @@
 import numpy as np
 
 
-def get_transformation():
+
+def get_transformation(points_reference, points_transformed):
+    """
+    Calculates the affine transformation between the points using least squares
+    :param points_reference: is a list of tuples representing the original points
+    :param points_transformed: is a list of tuples representing the points after transformation
+    :return:
+    """
     # points_reference, points_transformed
     points_reference = [[1, 1], [2, 2], [3, 3]]
     points_transformed = [[2, 2, 1], [3, 3, 1], [4, 4, 1]]
-    x = np.transpose(np.array(points_reference))
-    y = np.transpose(np.array(points_transformed))
-    print(x)
-    print(y)
-    a, b, a_rank, d = np.linalg.lstsq(x, y)
+    x = np.array(points_reference)
+    y = np.array(points_transformed)
+
+    parameters, _, a_rank, _ = np.linalg.lstsq(x, y)
+    a = parameters[:, [0, 1]]
+    b = parameters[:, [2]]
+
     print(a)
     print(b)
-    print(a_rank)
-    print(d)
-    print(np.multiply(a, np.matrix.transpose(x)))
+    print(np.dot(a, np.array([[3], [3]])) + b)
+
+    #print(np.array(parameters))
+    #a = np.delete(parameters, 2, 1)
 
 
+def test_transformation():
+    points_reference = [(1, 1), (2, 2), (3, 3)]
+    points_transformed = [(2, 2, 1), (3, 3, 1), (4, 4, 1)]
