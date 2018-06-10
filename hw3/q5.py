7944d4b77a53638e93c21f44143d8addbc7dcda1
BerIgor <bigor11@gmail.com>
2018-06-10 12:35:38 +0300

    Working on q5: interpolation is bad

--- hw3/q5.py
+++ hw3/q5.py
@@ -1,5 +1,20 @@
 import numpy as np
 from scipy import interpolate
+from hw3 import utils
+
+
+def perform(frames, transformations):
+    print("in q5.perform: number of frames: " + str(len(frames)))
+    stabilized_images = list()
+    for i in range(len(frames)):
+        print("handling frame: " + str(i))
+        frame = frames[i]
+        a, b = transformations[i]
+        stabilized_image = stabilize_image(frame, a, b)
+        stabilized_images.append(stabilized_image)
+        print(np.shape(stabilized_image))
+    print("in q5.perform: number of stabilized frames " + str(len(stabilized_images)))
+    return stabilized_images
 
 
 def stabilize_image(image, a, b):
@@ -12,12 +27,40 @@ def stabilize_image(image, a, b):
     """
     stabilized_image = np.zeros(np.shape(image))
 
+    y_cord = np.array(list(range(stabilized_image.shape[0])))
+    x_cord = np.array(list(range(stabilized_image.shape[1])))
+    stabilized_coordinates = np.array([[x_cord], [y_cord]])
+    print(stabilized_coordinates)
+    print(np.shape(stabilized_coordinates))
+    # Get the transformed coordinates: the coordinates in the ref image
+
+
+    source_coordinates = utils.transform(stabilized_coordinates, a, b)
+    print(np.shape(source_coordinates))
+    for layer_index in range(image.shape[-1]):
+        interpolation = interpolate.RectBivariateSpline(x, y, image[:, :, layer_index])
+
+#        stabilized_image[:, :, layer_index] = interpolate(x_transformed, y_transformed)
+
+    exit()
+    x_coordinates = list()
+    y_coordinates = list()
     for x in range(stabilized_image.shape[0]):
         for y in range(stabilized_image.shape[1]):
             current_coordinates = np.array([[x], [y]])
             transformed_coordinates = np.add(np.dot(a, current_coordinates), b)
             source_x = transformed_coordinates.item(0)
             source_y = transformed_coordinates.item(1)
-            # stabilized_image[new_x, new_y] = image[x, y]
-            stabilized_image[x, y] = interpolate.interp2d(source_x, source_y, image, kind='linear')
+            x_coordinates.append(source_x)
+            y_coordinates.append(source_y)
 
+    x = range(stabilized_image.shape[0])
+    y = range(stabilized_image.shape[1])
+
+    for layer_index in range(image.shape[-1]):
+        # utils.cvshow("LAYER", image[:, :, layer_index])
+        # stabilized_image = interpolate.interp2d(x_coordinates, y_coordinates, image[:, :, layer_index])
+        interpolation = interpolate.RectBivariateSpline(x, y, image[:, :, layer_index])
+        stabilized_image[:, :, layer_index] = interpolate()
+
+    return stabilized_image
--- hw3/utils.py
+++ hw3/utils.py
@@ -1,5 +1,33 @@
 import cv2 as cv
 import numpy as np
+import matplotlib.pyplot as plt
+
+
+ref_feature_points = [(297, 187), (303, 236), (447, 92), (421, 309), (459, 360), (505, 154)]
+frame1_feature_points = [(304, 148), (308, 199), (459, 38), (419, 256), (450, 299), (510, 116)]
+frame2_feature_points = [(280, 225), (283, 263), (439, 90), (390, 302), (424, 357), (503, 166)]
+frame3_feature_points = [(235, 234), (239, 297), (399, 97), (363, 341), (393, 387), (459, 193)]
+frame4_feature_points = [(238, 233), (243, 268), (412, 71), (361, 314), (391, 358), (468, 179)]
+frame5_feature_points = [(244, 239), (245, 292), (422, 85), (360, 318), (401, 382), (470, 209)]
+frame6_feature_points = [(229, 265), (234, 299), (411, 74), (348, 298), (387, 364), (459, 209)]
+
+frames_points = [ref_feature_points, frame1_feature_points, frame2_feature_points, frame3_feature_points,
+                 frame4_feature_points, frame5_feature_points, frame6_feature_points]
+
+
+def get_frames_points():
+    return frames_points
+
+
+def transform(coordinates, a, b):
+    """
+
+    :param coordinates: N coordinates in a matrix with shape 2xN
+    :param a: the a transformation matrix
+    :param b: the b transformation matrix
+    :return: a 2xN array of the transformed coordinates
+    """
+    return np.dot(a, coordinates) + b
 
 
 def cvshow(title, im):
@@ -34,3 +62,19 @@ def non_maximum_suppression(img, win_size):
 
     return suppressed_img, max_points_list
 
+
+def display_images(image_list, mode='RGB'):
+    f, sps = plt.subplots(nrows=1, ncols=len(image_list))
+    if mode == 'GRAY':
+        plt.gray()
+
+    for i in range(0, len(image_list)):
+        if mode == 'RGB':
+            image = np.flip(image_list[i], 2)
+        else:
+            image = image_list[i]
+        sps[i].imshow(image)
+        sps[i].axis('off')
+
+    plt.show()
+    return
